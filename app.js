9aadcc24ba4a83d22e808f20ddb61b73890fc968
Fatbardh <fatbardhemini5@gmail.com>
2020-05-01 13:31:01 -0700

    Final version without some bugs

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 // arrow.forEach(function(yy,index) {
 // 		yy.addEventListener("click", function(){
-// 			console.log("mire o o shoqi");
 // 			lowerpart.classList.toggle("show");
 // 			arrow[index].classList.toggle("rotate");
 // 			edge.classList.toggle("edge");
@@ -22,7 +21,6 @@ div.forEach(function(yy,index){
 	const b = a.children[0];
 	const expand = yy.children[1];
 	b.addEventListener("click", function(){
-			console.log("mire o o shoqi");
 			expand.classList.toggle("show");
 			b.classList.toggle("rotate");
 			b.style.transition = ' all 500ms linear';
@@ -31,8 +29,8 @@ div.forEach(function(yy,index){
 	});
 })
 
-const hamburger = document.querySelector('.header_list i')
-const pop = document.querySelector('.list_pop')
+const hamburger = document.querySelector('.header_list i');
+const pop = document.querySelector('.list_pop');
 const x = document.querySelector('.list_pop i');
 const a = document.querySelector('.list_pop ul');
 const body = document.querySelector('body');
